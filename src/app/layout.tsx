bd38bbc1acaf466b45e7c8f33149c1e0c7572093
SangMook-front <sm.kim@fanmaum.com>
2025-03-07 16:11:41 +0900

    fix: naver검색

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useServerInsertedHTML } from 'next/navigation';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
-import { ReactNode, useState } from 'react';
 import "@/styles/index.css";
+
+import { ReactNode, useState } from 'react';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+
+import { useServerInsertedHTML } from 'next/navigation';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
@@ -30,6 +32,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           content="새로운 시대! 새로운 세대! 신재생에너지, 스마트팜 등등 녹색성장을 통한 가치 창출"
         />
       </head>
+      <meta name="naver-site-verification" content="25aae90c6571989d45280f9e86257e6bbc497ea3" />
       <body>
         <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>
       </body>
