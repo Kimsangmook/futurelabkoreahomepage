b7d477049ee343a4a40e5740c34fb07247a2239d
Kimsangmook <uj03109@naver.com>
2025-06-02 21:46:19 +0900

    fix: text 수정

--- src/components/feature/LandingSection.tsx
+++ src/components/feature/LandingSection.tsx
@@ -1,11 +1,8 @@
 export default function LandingSection() {
   return (
     <section className="relative w-full h-screen">
-      {/* Background Image */}
       <div className="absolute inset-0 bg-[url('/background.png')] bg-center bg-cover" />
-      {/* Gradient Overlay */}
       <div className="absolute inset-0 bg-gradient-to-b from-white/80 via-white/40 to-transparent" />
-      {/* Content */}
       <div className="relative z-10 flex flex-col justify-center items-center h-full">
         <h1 className="text-gray-800 text-[32px] sm:text-[48px] font-bold leading-[48px] sm:leading-[64px] text-center">
           우리는 가장 가까운 텃밭,
